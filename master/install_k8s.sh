a8ec999fd5d9edd49b92f4fd479e8b63289d3db5
mendax-sh <marcoslucaspereira1@gmail.com>
2023-01-13 20:33:37 -0300

    Add files via upload

---
+++ master/install_k8s.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+apt update
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install -y apt-transport-https ca-certificates curl
+mkdir /etc/apt/keyrings/
+wget https://packages.cloud.google.com/apt/doc/apt-key.gpg
+mv apt-key.gpg  /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update -y
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+dpkg -i minikube_latest_amd64.deb
+rm minikube_latest_amd64.deb
+minikube config set driver docker
+minikube delete
+minikube start --force
+mkdir -p /home/vagrant/kubernetes/secao04
+cp -r /vagrant/secao04/ /home/vagrant/kubernetes/secao04
