26f184e5f36a998a9fc4ea48011e1c7c07738927
marcos <marcos@marcos>
2023-01-30 23:00:49 -0300

    v1

--- master/install_k8s.sh
+++ master/install_k8s.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 apt update
+mkdir /etc/apt/keyrings/
+
+apt-get install -y ca-certificates curl gnupg lsb-release apt-transport-https
+
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-apt-get install -y apt-transport-https ca-certificates curl
-mkdir /etc/apt/keyrings/
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
 wget https://packages.cloud.google.com/apt/doc/apt-key.gpg
 mv apt-key.gpg  /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
